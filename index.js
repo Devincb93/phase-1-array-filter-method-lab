1169fa637ed432c9dc24d11a1cb97cff03700f85
Devin <devincb93@gmail.com>
2023-07-15 16:51:03 -0700

    array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, obj) {
+        return drivers.filter(element => obj.toLowerCase() === element.toLowerCase())
+    }
+function fuzzyMatch(drivers, obj) {
+    return drivers.filter(element => obj[0] === element[0])
+}
+function matchName(drivers, obj) {
+    return drivers.filter(element => obj === element.name)
+}
